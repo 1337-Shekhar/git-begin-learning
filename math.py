a19b53614bf26c12acd44cb7eb8636c4f2527eff
shekhar <shekhar_verma@persistent.com>
2021-01-29 17:19:20 +0530

    Added implementation of division and added error in subtraction

--- math.py
+++ math.py
@@ -1,8 +1,10 @@
 def add(x,y):
     return x+y
 def subtract(x,y):
+    if x-y<0:
+        return ERROR
     return x-y
 def multiply(x,y):
     return x*y
 def divide(x,y):
-    pass
+    return x/y
